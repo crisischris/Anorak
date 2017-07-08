bf38d6d932cf0faafb3313d9a26d777b89e56a9b
chris nelson <christopherthomasnelson@gmail.com>
2017-07-08 15:40:57 -0700

    rewind via position
    tracking camera position to 'rewind' at end of level.

--- Assets/locomotion.cs
+++ Assets/locomotion.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class locomotion : MonoBehaviour {
 
-    public float speed = 5;
+    private float speed = 5;
+    private int TimeSinceStart;
+    private int CameraPosition;
+
+    
  
         void Update()
+
+
         {
            
             transform.Translate(Vector3.forward * Time.deltaTime*speed, Space.World);
+
+               if (transform.position.z > 100)
+                {
+                    speed = -5;
+                }
+               if (transform.position.z < 0)
+        {
+            speed = 5;
+        }
         }
 
     }
