a577c08dbef90cbf82aefb0528db30cc32b79be0
James Kim <james@firstbuild.com>
2017-07-11 23:01:06 -0400

    Modified TreeMaker such that object is not accessed after destruction.

--- Assets/scripts/TreeMaker.cs
+++ Assets/scripts/TreeMaker.cs
@@ -8,6 +8,7 @@ public class TreeMaker : MonoBehaviour {
     public GameObject Tree;
     private GameObject TreeInstance;
     public float speed = 2f;
+	private bool objectdestroyed;
 
 
     
@@ -18,40 +19,36 @@ public class TreeMaker : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-    var xPos = Random.Range(-5, 15);
-    var yPos = Random.Range(1.5f, 2);
-    var zPos = Random.Range(20, 40);
+	    var xPos = Random.Range(-5, 15);
+	    var yPos = Random.Range(1.5f, 2);
+	    var zPos = Random.Range(20, 40);
        
-
-       
-
-
         TreeInstance = GameObject.Instantiate(Tree, new Vector3(xPos, yPos, zPos), Quaternion.identity);
-
+		objectdestroyed = false;
 
     }
 
     // Update is called once per frame
     void Update () {
 
-       
-        var pos = TreeInstance.transform.position.z;
-
-        if (TreeInstance.transform.position.z > -20)
-
-        {          
-            TreeInstance.transform.Translate(new Vector3(0, 0, speed * -Time.deltaTime));       
-        }
-
-        print("the position of TreeInstance =" + pos);
-
-        
-        if (TreeInstance.transform.position.z < -20)
+		if (!objectdestroyed) {
+			moveTree ();
+		}
+			
+		if (!objectdestroyed && TreeInstance.transform.position.z < -20)
         {
             Destroy(TreeInstance);
+			objectdestroyed = true;
         }
-
-       
-
     }
+
+	void moveTree() {
+		var pos = TreeInstance.transform.position.z;
+
+		if (TreeInstance.transform.position.z > -20) {          
+			TreeInstance.transform.Translate (new Vector3 (0, 0, speed * -Time.deltaTime));       
+		}
+
+		print ("the position of TreeInstance =" + pos);
+	}
 }
