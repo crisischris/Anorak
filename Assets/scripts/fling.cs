77c0e78777381f84d006ec5175094f9be05ab885
James Kim <james@firstbuild.com>
2017-07-31 20:18:20 -0400

    Merging fling script with 3D primitive generation. Initial 'generation' value has been randomized per 'Start'.

--- Assets/scripts/fling.cs
+++ Assets/scripts/fling.cs
@@ -1,52 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class fling : MonoBehaviour {
+public class fling : MonoBehaviour
+{
 
-    bool isClicked = false;
-    public float vel;
-    public GameObject main;
-    public Rigidbody rb;
-    public Material defaultMat;
+	bool isClicked = false;
+	public float vel;
+	public GameObject main;
+	public Rigidbody rb;
 
-// Use this for initialization
-    void Start () {
-                
-        rb = GetComponent<Rigidbody>();
-    }
+	// Use this for initialization
+	void Start ()
+	{
+	}
         
 
-// Update is called once per frame
-    void Update () {
-        if (isClicked == true && transform.position.y <= 0 || transform.position.y >= 150 || transform.position.z >= 150)
-        {
-                Destroy(main);
-                print("destroyed!");
-                isClicked = false;
-            }
-        }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isClicked == true && transform.position.y <= 0 || transform.position.y >= 150 || transform.position.z >= 150) {
+			Destroy (main);
+			print ("destroyed!");
+			isClicked = false;
+		}
+	}
 
     
 
-    public void Clicked() {
-
-        Vector3 randomDirection = new Vector3(Random.value, Random.value, Random.value);
+	public void Clicked (BaseEventData eventData)
+	{
+		Vector3 randomDirection = new Vector3 (Random.value, Random.value, Random.value);
+		rb.AddForce (randomDirection * vel);
+		rb.useGravity = true;
+		isClicked = true;
+		print ("clicked!" + main.gameObject);
+		print (randomDirection);
         
-            rb.AddForce(randomDirection * vel);
-            rb.useGravity = true;
-            isClicked = true;
-            print("clicked!");
-            print(randomDirection);
-        
-    }
+	}
 
-    public void IsGazed() {
-                GetComponent<Renderer>().material.color  =  Color.red;
-    }
+	public void IsGazed (BaseEventData eventData)
+	{
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
 
 
-    public void IsNotGazed() {
-        GetComponent<Renderer>().material = defaultMat;
-    }
+	public void IsNotGazed (BaseEventData eventData)
+	{
+		GetComponent<Renderer> ().material.color = Color.yellow * Random.value;
+	}
 }
